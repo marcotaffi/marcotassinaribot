9d0fa943b42882cb579a40edcf7353868b83cb7b
Marco Tassinari <marco@taffi.it>
2024-12-03 08:11:12 +0000

    Aggiornamento generico

--- marcotassinaribot.js
+++ marcotassinaribot.js
@@ -1,33 +1,22 @@
 
-import BotChat from 'TaffiTools/src/bot/botchat.js';
+//import { BotChat } from 'TaffiTools/src/bot/botchat.js';
+import { BotNews } from 'TaffiTools/src/bot/botnews.js';
+import { Canale } from 'TaffiTools/src/bot/canale.js';
+
 import { ProcessManager } from 'TaffiTools/src/system/processmanager.js';
 import { debug } from 'TaffiTools/src/utils/debug.js';
 import { Servizi } from "TaffiTools/src/utils/servizi.js";
-import { SendMail, GoogleDrive } from "TaffiTools/src/api/ifttt.js"
-import { Console } from "TaffiTools/src/utils/console.js"
 import dotenv from 'dotenv';
 dotenv.config();
 
 
-// Funzione per caricare un prompt specifico dal file JSON corrispondente al nome del comando
-/*async function loadPrompt(command) {
-  try {
-    const filePath = path.join(__dirname, 'prompts', `${command}.json`);
-    const data = await fs.readFile(filePath, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    debug(1,"Error loading prompt:", error.message);
-    return null;
-  }
-}*/
-
-
+// IL CARICAMENTO DEI PROMPT DINAMICI DAL FILE E' GESTITO NELLA BOTCHAT.JS
 
 /**
- * La classe MarcoTassinariBot estende la classe BotChat e rappresenta un bot specifico.
- * Inizializza il bot con il token e il form di accoglienza specifico.
+ * La classe MarcoTassinariBot: l'avatar di Marco su Telegram
  */
-class BotMarcoTassinari extends BotChat {
+
+class BotMarcoTassinari extends BotNews {
   /**
    * Crea un'istanza di MarcoTassinariBot.
    */
@@ -37,17 +26,36 @@ class BotMarcoTassinari extends BotChat {
     const botToken = process.env.TELEGRAM_TAFFIBOT_TOKEN;
     const chatGptApiKey = process.env.OPENAI_API_KEY;
     const assistantID = process.env.ASSISTANT_ID; //l'assistente di questo bot
+    const iftttKey = process.env.IFTTT_WEBHOOKKEY;
+    const wordpressDipendenzeID = process.env.WORDPRESS_DIPENDENZE_ID
+
  
-
-
  //   let serviziPerMArco = new Servizi (["sendmail_root" , "sendmail_generic", "googledrive_filetest", "console_warn", "textedit_url_download"], process.env.IFTTT_WEBHOOKKEY ); //sevizi di esempio; mi passerò hookId. I servizi li saprò facendo una chiamata a chatgpt per sapere l'assistente cosa sa fare
-    let serviziPerMArco = new Servizi (["console_log", "textedit_url_download", "wordpress_dipendenze_elencaArticoli"], process.env.IFTTT_WEBHOOKKEY ); //sevizi di esempio; mi passerò hookId. I servizi li saprò facendo una chiamata a chatgpt per sapere l'assistente cosa sa fare
  
-    
-//    serviziPerMArco.start(); NON SERVE
+const prompts = {
+   ripubblicaNotizia : "Devi scrivere un articolo di rilancio di una notizia. Prima di iniziare chiedi se hai già il materiale necessario oppure se l'utente vuole inviarti contenuti. Riscrivi un articolo giornalistico da questo materiale, modificando il testo originale secondo queste indicazioni: - non riscrivere il titolo, ti serve solo come contesto per l'articolo; - modifica l'ordine dei contenuti per raggruppare i temi simili; - metti in evidenza subito la notizia principale; - adegua il tono di voce allo stile sobrio e moderato di semprenews.it; - evita di ripetere le frasi date, ma modificale utilizzando sinonimi e stile per rendere meno riconoscibile il testo originale; Voglio un testo lungo: usa tutto il materiale che ti viene fornito, comprendendo la notizia, gli approfondimenti, i dati, le citazioni, le storie e gli esempi riportati. Alla fine aggiungi una dicitura simile a questa: Per saperne di più leggi l'articolo originale su [Nome Testata](url).",
+   scriviTitoli : "Devi generare dei titoli per il portale online semprenews.it. Prima di iniziare chiedi se hai già il materiale necessario oppure se l'utente vuole inviarti contenuti. Genera un set di titoli che aiutino il lettore a comprendere gli elementi chiave dell'articolo dato. I titoli devono essere rispettosi e descrittivi, e introdurre ad aspetti più curiosi e popolari dell'articolo.",
+}
 
-      super(chatGptApiKey, assistantID, botToken, serviziPerMArco ); 
- 
+
+       let sitoDipendenze = new Canale("wordpress_dipendenzepatologiche.apg23.org", wordpressDipendenzeID, "https://dipendenzepatologiche.apg23.org",chatGptApiKey);
+       let sitoSempre = new Canale("sendmail_sempreredazione@apg23.org",iftttKey, "sempreredazione@apg23.org",chatGptApiKey);
+
+       sitoDipendenze.setFeed(["dipendenze"]);
+       sitoSempre.setFeed(["politica", "sport"]).setAuto().setPrompts(prompts);
+       // servizi generici
+       let serviziPerMArco = new Servizi (["console_log", "textedit_url_download", "googledrive_filetest"], process.env.IFTTT_WEBHOOKKEY ); //sevizi di esempio; mi passerò hookId. I servizi li saprò facendo una chiamata a chatgpt per sapere l'assistente cosa sa fare
+
+       serviziPerMArco.registraServizio(sitoDipendenze.servizio); //aggiungo questi due servizi: pubblicazione sui canali
+       serviziPerMArco.registraServizio(sitoSempre.servizio);
+       
+//- Nota: tutti questi servizi devono essere già presenti nell'assistente
+
+       super(chatGptApiKey, assistantID, botToken, serviziPerMArco ); 
+
+
+      this.aggiungiCanali([sitoDipendenze,sitoSempre]); //aggiunge i contesti al dialogo attuale
+
     }
 
 
@@ -68,7 +76,7 @@ class BotMarcoTassinari extends BotChat {
     debug(0,"Avvio il server...", process.env.DEBUG_LEVEL);
     debug(0, "debug level:", DEBUG_LEVEL);
 
-    new BotMarcoTassinari().start();
+    new BotMarcoTassinari().followFeeds().start();
   } catch (error) {
     debug(1,`Errore nell'avvio del bot Marco Tassinari:`, error);
   }
