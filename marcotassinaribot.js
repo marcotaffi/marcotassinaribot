4704641f756290f8413f1fa4189d897833586c53
Marco Tassinari <marco@taffi.it>
2024-12-07 15:26:27 +0000

    Aggiornamento generico

--- marcotassinaribot.js
+++ marcotassinaribot.js
@@ -2,7 +2,6 @@
 //import { BotChat } from 'TaffiTools/src/bot/botchat.js';
 import { BotNews } from 'TaffiTools/src/bot/botnews.js';
 import { Canale } from 'TaffiTools/src/bot/canale.js';
-
 import { ProcessManager } from 'TaffiTools/src/system/processmanager.js';
 import { debug } from 'TaffiTools/src/utils/debug.js';
 import { Servizi } from "TaffiTools/src/utils/servizi.js";
@@ -17,6 +16,7 @@ dotenv.config();
  */
 
 class BotMarcoTassinari extends BotNews {
+
   /**
    * Crea un'istanza di MarcoTassinariBot.
    */
@@ -27,34 +27,52 @@ class BotMarcoTassinari extends BotNews {
     const chatGptApiKey = process.env.OPENAI_API_KEY;
     const assistantID = process.env.ASSISTANT_ID; //l'assistente di questo bot
     const iftttKey = process.env.IFTTT_WEBHOOKKEY;
-    const wordpressDipendenzeID = process.env.WORDPRESS_DIPENDENZE_ID
+   // const wordpressDipendenzeID = process.env.WORDPRESS_DIPENDENZE_ID
 
  
  //   let serviziPerMArco = new Servizi (["sendmail_root" , "sendmail_generic", "googledrive_filetest", "console_warn", "textedit_url_download"], process.env.IFTTT_WEBHOOKKEY ); //sevizi di esempio; mi passerò hookId. I servizi li saprò facendo una chiamata a chatgpt per sapere l'assistente cosa sa fare
  
 const prompts = {
-   ripubblicaNotizia : "Devi scrivere un articolo di rilancio di una notizia. Prima di iniziare chiedi se hai già il materiale necessario oppure se l'utente vuole inviarti contenuti. Riscrivi un articolo giornalistico da questo materiale, modificando il testo originale secondo queste indicazioni: - non riscrivere il titolo, ti serve solo come contesto per l'articolo; - modifica l'ordine dei contenuti per raggruppare i temi simili; - metti in evidenza subito la notizia principale; - adegua il tono di voce allo stile sobrio e moderato di semprenews.it; - evita di ripetere le frasi date, ma modificale utilizzando sinonimi e stile per rendere meno riconoscibile il testo originale; Voglio un testo lungo: usa tutto il materiale che ti viene fornito, comprendendo la notizia, gli approfondimenti, i dati, le citazioni, le storie e gli esempi riportati. Alla fine aggiungi una dicitura simile a questa: Per saperne di più leggi l'articolo originale su [Nome Testata](url).",
-   scriviTitoli : "Devi generare dei titoli per il portale online semprenews.it. Prima di iniziare chiedi se hai già il materiale necessario oppure se l'utente vuole inviarti contenuti. Genera un set di titoli che aiutino il lettore a comprendere gli elementi chiave dell'articolo dato. I titoli devono essere rispettosi e descrittivi, e introdurre ad aspetti più curiosi e popolari dell'articolo.",
+   ripubblicaNotizia : "Devi scrivere un articolo di rilancio di una notizia, partendo da uno o più articoli dati. Scrivi un articolo giornalistico da questo materiale, modificando i contenuti originali secondo queste indicazioni: - metti in evidenza subito la notizia principale cambiando l'attacco;  - modifica l'ordine dei contenuti per raggruppare i temi simili; - adegua il tono di voce allo stile sobrio e moderato di semprenews.it; - evita di ripetere le frasi date, ma modificale utilizzando sinonimi e stili per rendere meno riconoscibile il testo originale; Voglio un testo lungo: usa tutto il materiale che ti viene fornito, comprendendo la notizia, gli approfondimenti, i dati, le citazioni, le storie e gli esempi riportati. Dove necessario aggiungi diciture simili a questaz: Per saperne di più leggi l'articolo originale su [Nome Testata](url). Non inserire i contenti ma dammi solo il testo finale. Usa <br><br> come fine riga.",
+   scriviTitoli : "Devi generare dei titoli per il portale online semprenews.it. Genera i titoli per l'articolo dato, che aiutino il lettore a comprendere gli elementi chiave del pezzo. Sono richiesti: titolo breve, occhiello, sommario, titolo SEO, meta description.  I titoli devono essere rispettosi e descrittivi, e introdurre ad aspetti più curiosi e popolari dell'articolo. Voglio i titoli espressi come coppia chiave:valore separati da <br><br>",
 }
 
 
-       let sitoDipendenze = new Canale("wordpress_dipendenzepatologiche.apg23.org", wordpressDipendenzeID, "https://dipendenzepatologiche.apg23.org",chatGptApiKey);
-       let sitoSempre = new Canale("sendmail_sempreredazione@apg23.org",iftttKey, "sempreredazione@apg23.org",chatGptApiKey);
+    //   let sitoDipendenze = new Canale("wordpress_dipendenzepatologiche.apg23.org_creaArticolo", wordpressDipendenzeID, "https://dipendenzepatologiche.apg23.org",chatGptApiKey);
+      // let sitoSempre = new Canale("sendmail_sempreredazione@apg23.org_mailto",iftttKey, "sempreredazione@apg23.org",chatGptApiKey);
+      let sitoSempreEcologia = new Canale("sendmail_redazionesempre_mailto","marcotassinari@apg23.org",iftttKey, chatGptApiKey);
+      let sitoSempreMondo = new Canale("sendmail_redazionesempre_mailto", "marcotassinari@apg23.org",iftttKey,chatGptApiKey);
+      let sitoSempreSolidarietà = new Canale("sendmail_redazionesempre_mailto", "marcotassinari@apg23.org",iftttKey,chatGptApiKey);
+      let sitoSempreFamiglia = new Canale("sendmail_redazionesempre_mailto", "marcotassinari@apg23.org",iftttKey,chatGptApiKey);
+       let sitoSempreCultura = new Canale("sendmail_redazionesempre_mailto", "marcotassinari@apg23.org",iftttKey,chatGptApiKey);
+       let sitoSempreSpiritualità = new Canale("sendmail_redazionesempre_mailto", "marcotassinari@apg23.org",iftttKey,chatGptApiKey);
 
-       sitoDipendenze.setFeed(["dipendenze"]);
-       sitoSempre.setFeed(["politica", "sport"]).setAuto().setPrompts(prompts);
-       // servizi generici
+       sitoSempreEcologia.setFeed(["economia","ambiente","scienza", "tecnologia"]).setPrompts(prompts);
+       sitoSempreMondo.setFeed(["pace"]).setPrompts(prompts);
+       sitoSempreSolidarietà.setFeed(["dipendenze", "giustizia", "volontariato","migranti","prostituzione","strada"]).setPrompts(prompts);
+       sitoSempreFamiglia.setFeed(["disabilità","educazione","infanzia", "salute","famiglia","vita","affido"]).setPrompts(prompts);
+       sitoSempreCultura.setFeed(["politica", "sport", "diritti umani", "spettacoli","alimentazione"]).setPrompts(prompts);
+       sitoSempreSpiritualità.setFeed(["religione"]).setPrompts(prompts);
+
+/*       sitoSempreSolidarietà.setFeed(["dipendenze", "giustizia", "disabilità", "educazione"]);
+       sitoSempre.setFeed(["politica", "sport", "pace", "economia", "infanzia", "religione"]).setAuto().setPrompts(prompts);
+  */
+      // servizi generici
        let serviziPerMArco = new Servizi (["console_log", "textedit_url_download", "googledrive_filetest"], process.env.IFTTT_WEBHOOKKEY ); //sevizi di esempio; mi passerò hookId. I servizi li saprò facendo una chiamata a chatgpt per sapere l'assistente cosa sa fare
 
-       serviziPerMArco.registraServizio(sitoDipendenze.servizio); //aggiungo questi due servizi: pubblicazione sui canali
-       serviziPerMArco.registraServizio(sitoSempre.servizio);
-       
+       serviziPerMArco.registraServizio(sitoSempreEcologia.servizio); //aggiungo questi due servizi: pubblicazione sui canali
+       serviziPerMArco.registraServizio(sitoSempreMondo.servizio);
+       serviziPerMArco.registraServizio(sitoSempreSolidarietà.servizio);
+       serviziPerMArco.registraServizio(sitoSempreFamiglia.servizio);
+       serviziPerMArco.registraServizio(sitoSempreCultura.servizio);
+       serviziPerMArco.registraServizio(sitoSempreSpiritualità.servizio);
+
 //- Nota: tutti questi servizi devono essere già presenti nell'assistente
 
        super(chatGptApiKey, assistantID, botToken, serviziPerMArco ); 
 
 
-      this.aggiungiCanali([sitoDipendenze,sitoSempre]); //aggiunge i contesti al dialogo attuale
+      this.aggiungiCanali([sitoSempreEcologia,sitoSempreMondo,sitoSempreSolidarietà,sitoSempreFamiglia,sitoSempreCultura,sitoSempreSpiritualità]); //aggiunge i contesti al dialogo attuale
 
     }
 
