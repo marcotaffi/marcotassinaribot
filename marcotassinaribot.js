1356b1ea0f1ede62f3bf500d9a029fec152318a1
Marco Tassinari <marco@taffi.it>
2025-03-08 20:33:46 +0000

    Aggiornamento generico

--- marcotassinaribot.js
+++ marcotassinaribot.js
@@ -45,10 +45,10 @@ const DEBUG_LEVEL = process.env.DEBUG_LEVEL;
 const propmtLinkedin = {
   postLinkedin: 
 { prompt:
-`Scrivi un post molto breve per la pagina personale su Linkedin di Marco Tassinari. Il post esprime un commento puntuale e sintetico sul testo dato. 
+`Scrivi un post molto breve per la pagina personale su Linkedin di Marco Tassinari. Il post è costituito da una o due frasi e può esprimere un commento puntuale e sintetico sul testo dato. 
 Lo stile deve essere empatico, colloquiale ed informale, scritto con vocaboli semplici e frasi brevi. Non usare punti esclamativi o toni enfatici, ma preferisci un testo in generale moderato, anche se a tratti impulsivo. 
 
-Scegli un criterio fra questi per la scrittura del breve commento:
+Puoi scegliere un criterio fra questi per la scrittura del breve commento:
 
 - Marco guada con speranza al futuro, alle novità con cui il genere umano costruirà un mondo più giusto ed umano in cui i propri figli potranno vivere in armonia e realizzare la pace. 
 - Marco in generale ritiene che sia utile mettersi a capofitto a sperimentare sulle nuove possibilità offerte dalle recenti scoperte, dall'innovazione tecnologica e dai progressi dell'umanità, perché è necessario conoscerli per poterli controllare. 
@@ -56,8 +56,9 @@ Scegli un criterio fra questi per la scrittura del breve commento:
 - Le nuove tecnologie rischiano di mettere a rischio le democrazie e di favorire i governi autoritari.
 - Marco pensa che il cambiamento dei prossimi anni sarà repentino: ritiene che verrà trasformato repentinamente l'attuale flusso di notizie cui siamo abituali; soprattutto le fasce più fragili della popolazione dovranno dotarsi di strumenti nuovi per rimanere al passo.
 - Marco ama le descrizioni tecnologiche. Programma in node.js su Linux Debian chiamando le API di ChatGpt e di molti altri servizi, realizzando bot in AI e creando soluzioni per l'automazione avanzata.
+- Eventali altri punti di vista relativi alla biografia di Marco
 
-Inizia subito con il commento senza preamboli od introduzioni, e taglia in maniera netta senza conclusioni o rimandi. 
+Inizia subito con il commento senza preamboli od introduzioni, e taglia subito in maniera netta senza conclusioni o rimandi. 
 
 Esempi: 
 - Xiaomi ha presentato al MWC 2025 una serie di prodotti che dimostrano la loro forza e visione. Sono curioso di vedere come influenzeranno il nostro modo di informarci e interagire.
@@ -67,6 +68,7 @@ Esempi:
 - I video POV generati dall'intelligenza artificiale stanno cambiando il modo in cui interagiamo con i contenuti visivi. Quanto tecnologie sostenute da miliardi di investimenti trasformeranno il nostro modo di vivere?
 - L'Europa finirà in secondo piano? La crescente dipendenza dalle infrastrutture digitali americane evidenzia quanto sia urgente investire in un'autonomia tecnologica. Ci serve una strategia di innovazione che salvaguardi la libertà dell'individuo e la sovranità dei dati.
 - OpenAI lancia il modello 5: lo aspettavamo tutti. Novità principale è la capacità di elaborazione parallela dei prompt, che supera il concetto di token cui eravamo abituati. Dico subito ai miei bot di testarla.
+- Ci sono lavori che l'AI non farà mai: da domani, tutti pasticcieri!
 `
 },
 params: {assistant_id: assistantID,} 
